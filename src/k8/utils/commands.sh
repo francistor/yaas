d682ee6717b438b321d3cbc9f6be69abdfa5e0b0
francistor <francisco.cardosogil@gmail.com>
2019-05-04 14:53:34 +0200

    Kubernetes testing

--- src/k8/utils/commands.sh
+++ src/k8/utils/commands.sh
@@ -1,9 +1,26 @@
+# Environment
+aaaserver=$HOME/Yaas/target/universal/stage/bin/aaaserver
+k8clientConfig=$HOME/Yaas/src/k8/conf/client
+
+# Deploy
+kubectl create -f Yaas/src/k8/descriptors/kagent.yml
+
+# Reload servers
+kubectl scale --replicas=0 statefulSet/yaas-server
+kubectl scale --replicas=0 statefulSet/yaas-superserver
+kubectl scale --replicas=2 statefulSet/yaas-server
+kubectl scale --replicas=2 statefulSet/yaas-superserver
+
 # Peer status
 
 # Server
-kubectl exec -it busybox -- wget -q -O- http://yaas-server-0.yaas-server:19000/diameter/peers | jq .
-kubectl exec -it busybox -- wget -q -O- http://yaas-server-1.yaas-server:19000/diameter/peers | jq .
+kubectl exec -it kagent -- wget -q -O- http://yaas-server-0.yaas-server:19000/diameter/peers | jq .
+kubectl exec -it kagent -- wget -q -O- http://yaas-server-1.yaas-server:19000/diameter/peers | jq .
 
 # Superserver
-kubectl exec -it busybox -- wget -q -O- http://yaas-superserver-0.yaas-superserver-diameter:19000/diameter/peers | jq .
-kubectl exec -it busybox -- wget -q -O- http://yaas-superserver-1.yaas-superserver-diameter:19000/diameter/peers | jq .
+kubectl exec -it kagent -- wget -q -O- http://yaas-superserver-0.yaas-superserver-diameter:19000/diameter/peers | jq .
+kubectl exec -it kagent -- wget -q -O- http://yaas-superserver-1.yaas-superserver-diameter:19000/diameter/peers | jq .
+
+# Launch radius client
+$aaaserver -Dinstance=radius -Dconfig.file=${k8clientConfig}/aaa-default.conf -Dlogback.configurationFile=${k8clientConfig}/logback-default.xml
+
